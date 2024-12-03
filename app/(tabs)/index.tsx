71e592b9fb10d80799db44e008e1ebe690e11c2b
Cadari <cadari@flexdev.com.br>
2024-12-03 11:51:41 -0300

    :sparkles: feat: Modal Stickers
    Modal que adiciona stickers para serem selecionados pelo usuário. Os stickers são baseados em emojis

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
-import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
-import { Image } from "expo-image";
-import ImageViewer from "../components/ImageViewer";
-import Button from "../components/Button";
+import { View, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
+import Button from "../components/Button";
+import ImageViewer from "../components/ImageViewer";
+import IconButton from "../components/IconButton";
+import CircleButton from "../components/CircleButton";
+import EmojiPicker from "../components/EmojiPicker";
+import { type ImageSource } from "expo-image";
+import EmojiList from "../components/EmojiList";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -12,8 +15,13 @@ export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined,
   );
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(
+    undefined,
+  );
 
-  const pickImage = async () => {
+  const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
       allowsEditing: true,
@@ -22,10 +30,25 @@ export default function Index() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
-      alert("Tou did not select any image");
+      alert("You did not select any image.");
     }
   };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {};
 
   return (
     <View style={styles.container}>
@@ -35,14 +58,37 @@ export default function Index() {
           selectedImage={selectedImage}
         />
       </View>
-      <View style={styles.footerContainer}>
-        <Button
-          label="Choose a photo"
-          theme="primary"
-          event={pickImage}
-        ></Button>
-        <Button label="Use this photo"></Button>
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            event={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            event={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList
+          onSelect={setPickedEmoji}
+          onCloseModal={onModalClose}
+        ></EmojiList>
+      </EmojiPicker>
     </View>
   );
 }
@@ -53,19 +99,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     alignItems: "center",
   },
-  text: {
-    color: "#fff",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
-  },
   imageContainer: {
     flex: 1,
   },
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
